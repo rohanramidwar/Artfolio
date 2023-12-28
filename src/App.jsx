b946633183355e1a5c3b0951e42c6dd13caa94ed
RohanRamidwar <rohanramidwar71@gmail.com>
2023-12-28 14:17:30 +0530

    reconstruct

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import Home from "./pages/Home";
-import CreateArea from "./pages/CreateArea";
-import Navbar from "./components/Navbar";
-import { Route, Routes } from "react-router-dom";
-import Cart from "./pages/Cart";
+import React from "react";
 
 function App() {
-  return (
-    <div>
-      <div>
-        <Navbar />
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/submit" element={<CreateArea />} />
-        <Route path="/cart" element={<Cart />}></Route>
-      </Routes>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
