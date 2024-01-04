9cdaa36ad222dbde996b24772badba6ee0de368d
RohanRamidwar <rohanramidwar71@gmail.com>
2024-01-05 01:38:49 +0530

    add feedback section

---
+++ src/components/UserFeedbackSection.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { SendHorizontal } from "lucide-react";
+import SingleComment from "./SingleComment";
+
+const UserFeedbackSection = () => {
+  return (
+    <div className="p-6 border-l-2 md:pl-[53px] border-[#2b2d31]">
+      <div className="flex flex-col w-[300px]">
+        <h2 className="font-semibold text-xl mb-4">Feedback</h2>
+
+        <label className="relative flex items-center mb-6">
+          <button className="absolute right-2">
+            <SendHorizontal width={20} />
+          </button>
+          <textarea
+            className="bg-[#1e1f22] p-2 w-full outline-none rounded"
+            placeholder="add your thoughts"
+          />
+        </label>
+
+        <SingleComment />
+      </div>
+    </div>
+  );
+};
+
+export default UserFeedbackSection;
