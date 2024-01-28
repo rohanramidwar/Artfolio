1972452236bca62e179927481a1ef512c7719faa
RohanRamidwar <rohanramidwar71@gmail.com>
2024-01-28 11:44:46 +0530

    adjust search bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,42 @@
-import { React, useState } from "react";
-import { Avatar } from "@mui/material";
-
 //font
-import "@fontsource/inter";
-import "@fontsource/inter/400.css";
-
-//logo
-import AppLogo from "./assets/Artfolio-logos_black.png";
+import "@fontsource/karla";
+import "@fontsource/karla/600.css";
+//user photo
+import UserPhoto from "./assets/user_img.png";
+//lucide icons
+import { Search } from "lucide-react";
 
 function App() {
   return (
     <div>
       <nav className="flex justify-between p-10">
-        <div>
-          <img src={AppLogo} width={101} alt="Artfolio" />
+        <div className="flex gap-6 items-center">
+          <h2 className="font-semibold text-2xl ">LOGO</h2>
+          <ul className="flex gap-6">
+            <li>Find artist</li>
+            <li>Inspiration</li>
+            <li>Learn art</li>
+            <li>Workshops</li>
+          </ul>
         </div>
 
-        <div className="flex gap-4">
-          <button>
-            <Avatar>R</Avatar>
-          </button>
-          <button className="shadow-lg bg-purple-600 p-2.5 text-white rounded-full  ">
-            Log out
-          </button>
+        <div className="flex gap-4 items-center">
+          <div className="relative">
+            <input
+              className="pl-[48px] py-[16px] pr-[24px] outline-none rounded-full bg-gray-100"
+              placeholder="Search..."
+            />
+            <label className="absolute left-[24px] top-[16px]">
+              <Search size={16} />
+            </label>
+          </div>
+          <div>
+            <img
+              className="rounded-full w-[45.33px]"
+              src={UserPhoto}
+              alt="Avatar"
+            />
+          </div>
         </div>
       </nav>
     </div>
