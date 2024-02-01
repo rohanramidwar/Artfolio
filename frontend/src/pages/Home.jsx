26c7f37866e64f5a99167fe06eeb385d46de36f1
RohanRamidwar <rohanramidwar71@gmail.com>
2024-02-01 20:08:47 +0530

    edit profile, share work form added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,32 +1,80 @@
-import React from "react";
+import React, { useRef } from "react";
 import ProjectCard from "../components/ProjectCard";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 function Home() {
+  const categories = [
+    "Discover",
+    "Anime",
+    "Sketches",
+    "Anatomy",
+    "Realism",
+    "Comic art",
+    "Tutorials",
+  ];
+
+  const categoriesRef = useRef(null);
+
+  function handleScroll() {
+    const container = categoriesRef.current;
+
+    document.getElementById("leftChevron").style.visibility =
+      container.scrollLeft > 0 ? "visible" : "hidden";
+  }
+
+  function scrollToLeft() {
+    const container = categoriesRef.current;
+
+    container.scrollTo({
+      left: container.scrollLeft - 340,
+      behavior: "smooth",
+    });
+  }
+
+  function scrollToRight() {
+    const container = categoriesRef.current;
+
+    container.scrollTo({
+      left: container.scrollLeft + 340,
+      behavior: "smooth",
+    });
+  }
+
   return (
     <div className="flexCenter flex-col text-gray-700 mx-[72px]">
-      <div className="flexCenter mt-10 flex gap-2 w-2/3 ">
-        <button className="px-4 h-9  rounded-full font-semibold hover:text-gray-600  focus:bg-[#f8f7f4] active:bg-[#f8f7f4] ">
-          Discover
-        </button>
-        <button className="px-4 h-9  rounded-full font-semibold hover:text-gray-600  focus:bg-[#f8f7f4] active:bg-[#f8f7f4] ">
-          Anime
-        </button>
-        <button className="px-4 h-9  rounded-full font-semibold hover:text-gray-600  focus:bg-[#f8f7f4] active:bg-[#f8f7f4] ">
-          Sketches
-        </button>
-        <button className="px-4 h-9  rounded-full font-semibold hover:text-gray-600  focus:bg-[#f8f7f4] active:bg-[#f8f7f4] ">
-          Anatomy
-        </button>
-        <button className="px-4 h-9  rounded-full font-semibold hover:text-gray-600  focus:bg-[#f8f7f4] active:bg-[#f8f7f4] ">
-          Realism
-        </button>
-        <button className="px-4 h-9  rounded-full font-semibold hover:text-gray-600  focus:bg-[#f8f7f4] active:bg-[#f8f7f4] ">
-          Comic art
-        </button>
-        <button className="px-4 h-9  rounded-full font-semibold hover:text-gray-600  focus:bg-[#f8f7f4] active:bg-[#f8f7f4] ">
-          Tutorials
-        </button>
+      <div className="relative flexCenter mt-10 w-2/3 overflow-x-hidden scroll-smooth">
+        <div
+          role="button"
+          onClick={scrollToLeft}
+          id="leftChevron"
+          className="absolute invisible left-0 bg-gradient-to-r from-white to-transparent pr-20"
+        >
+          <ChevronLeft size={20} />
+        </div>
+
+        <div
+          ref={categoriesRef}
+          onScroll={handleScroll}
+          className="flex gap-2 whitespace-nowrap overflow-x-hidden scroll-smooth mx-4"
+        >
+          {categories.map((category, index) => (
+            <button
+              key={index}
+              className="px-4 h-9 rounded-full font-semibold hover:text-gray-600 focus:bg-purple-100 active:bg-purple-100"
+            >
+              {category}
+            </button>
+          ))}
+        </div>
+
+        <div
+          id="rightChevron"
+          className="absolute visible right-0 bg-gradient-to-l from-white to-transparent pl-20"
+        >
+          <ChevronRight size={20} onClick={scrollToRight} />
+        </div>
       </div>
+
       <div className="mt-10 grid grid-cols-3 gap-9">
         <ProjectCard />
       </div>
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,31 +1,42 @@
 import React from "react";
-import avatarUrl from "../assets/user-photo.png";
 import { Link } from "react-router-dom";
+import EditProfile from "../components/EditProfile";
 
 const ProfilePage = () => {
   return (
-    <div className="my-10 text-gray-700 flexCenter flex-col gap-10">
+    <div className="mb-10 text-gray-700 flexCenter flex-col gap-10">
       <div className=" flexCenter flex-col w-2/5">
         <img
-          className="rounded-full "
-          width={120}
-          height={120}
-          src={avatarUrl}
+          className="rounded-full w-[120px] h-[120px]"
+          src="https://i.pinimg.com/564x/4b/cc/54/4bcc54ebe6d0e6700e3df3047c1129c8.jpg"
           alt="profile photo"
         />
 
-        <p className="mt-3 font-semibold text-3xl">Rohan</p>
-        <p className="text-center">hi! I'm Portrait artist based in India 🎨</p>
+        <p className="my-3 font-semibold text-4xl">Raj</p>
+        <p className="text-center">
+          Hello! I'm a passionate portrait artist from India
+        </p>
         <a href="#" className="font-semibold hover:underline text-center">
-          instagram.com/rohan1208
+          instagram.com/raj112
         </a>
-        <button className="mt-3 p-2 px-4 bg-purple-600 hover:bg-purple-700 rounded-xl shadow text-white font-semibold">
-          Follow
-        </button>
+        {/* <div className="flexCenter text-sm gap-3">
+          <button className="mt-2 px-4 h-9  text-white bg-gray-800 hover:bg-gray-700 rounded-full  font-semibold ">
+            Get in touch
+          </button>
+          <button className="mt-2 px-4 h-9 border border-gray-200 hover:border-gray-300 rounded-full  font-semibold ">
+            Follow
+          </button>
+        </div> */}
+
+        <Link to={"/editProfile"}>
+          <button className="mt-2 px-4 h-9 border border-gray-200 hover:border-gray-300 rounded-full  font-semibold ">
+            Edit Profile
+          </button>
+        </Link>
       </div>
 
       <div className="flex flex-col">
-        <h1 className="font-semibold text-xl">Work</h1>
+        <h2 className="font-semibold px-4">Work</h2>
         <div className="mt-8 pt-12 grid grid-cols-3 gap-9 border-t border-gray-300">
           <div className="flexCenter flex-col rounded-2xl drop-shadow-card w-[349px]">
             <Link
