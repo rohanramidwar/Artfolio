dbe90a68bce6643c95e82a6984beba8815320388
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-04-23 20:08:12 +0530

    checked routes on client side

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -3,16 +3,16 @@ import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 
 import profilePic from "../assets/profilePic.png";
 
-const Post = ({ src }) => {
+const Post = ({ title, desc }) => {
   return (
     <div className="flex flex-col gap-2 w-full mb-5 break-inside-avoid">
       <Drawer>
         <DrawerTrigger>
-          <img className="max-w-full rounded-xl" src={src} alt="art" />
+          <img className="max-w-full rounded-xl" alt="art" />
         </DrawerTrigger>
         <DrawerContent className="h-[91%]"> </DrawerContent>
       </Drawer>
-      <p className="font-medium text-sm">Art by lovearridle_</p>
+      <p className="font-medium text-sm">{title}</p>
       <div className="cursor-pointer flex gap-2 items-center">
         <img className="w-8 h-8" src={profilePic} alt="profile-pic" />
         <p className="hover:underline text-sm">Artist</p>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,14 +5,12 @@ import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  const [posts, setPosts] = useState();
+  const [posts, setPosts] = useState([]);
   console.log("res", posts);
 
   const fetchPosts = async () => {
     try {
-      const res = await axios.get("http://localhost:5000/posts", {
-        withCredentials: true,
-      });
+      const res = await axios.get("http://localhost:5000/posts/");
       setPosts(res);
     } catch (error) {
       console.log(error);
@@ -36,7 +34,7 @@ const Home = () => {
       <Link to="/auth">
         <Button>Sign in</Button>
       </Link>
-      <HomeLayout />
+      <HomeLayout posts={posts} />
     </div>
   );
 };
