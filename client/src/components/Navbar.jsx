aa34970dc81c1441be9417f326740a26e3b0ab84
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-05-14 00:51:40 +0530

    navbar ui add

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,40 @@
 import React from "react";
 import appLogo from "../assets/folio.png";
+import { Search } from "lucide-react";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
-    <nav className="px-5">
-      <img className="w-20" src={appLogo} alt="Artfolio" />
-      <button>Create</button>
+    <nav className="bg-white z-10 fixed w-full flex justify-between p-5 text-gray-800">
+      <div className="flex gap-7">
+        <img className="w-16    " src={appLogo} alt="Artfolio" />
+        <Button className="rounded-full font-semibold h-12">Create</Button>
+      </div>
+
+      <div className="mx-7 relative w-4/5">
+        <Search className="absolute top-3 left-6 text-gray-400" size={20} />
+        <input
+          className="bg-gray-100 w-full rounded-full h-12 pl-12 pr-6 outline-none placeholder:text-gray-400"
+          placeholder="Search..."
+        />
+      </div>
+      {/* right  */}
+      <div className="flex gap-5">
+        {/* <div>
+          <img
+            className="w-12 h-12 rounded-full"
+            src="https://i.pinimg.com/280x280_RS/dd/73/58/dd73584f718d8fa359c5b8972baa7434.jpg"
+            alt="user"
+          />
+        </div> */}
+
+        {/* sign in  */}
+        <div>
+          <Button className="h-12 rounded-full font-semibold bg-purple-500 hover:bg-purple-600">
+            Sign in
+          </Button>
+        </div>
+      </div>
     </nav>
   );
 };
