69326e21f7256fc886905d873fe0c736839e8080
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-05-16 23:51:14 +0530

    create page ui

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -18,23 +18,24 @@ const CreatePost = () => {
       </div>
 
       {/* inputs  */}
-      <div className="overflow-auto scroll-smooth pt-10 pb-20 flex flex-col gap-5 items-center mt-20">
-        <input
-          placeholder="Give me a name"
-          className="w-1/3 p-3 outline-none border focus:border-zinc-500 rounded-lg"
-        />
-        <div
-          role="button"
-          className="text-slate-500 flex flex-col gap-3 justify-center items-center w-1/3 min-h-60 border-2 border-dashed rounded-lg "
-        >
-          <img width={100} src={picPlaceholder} alt="pic-placeholder" />
-          <p className="text-sm">Browse art</p>
+      <div className="flex items-center justify-center">
+        <div className="w-2/5 overflow-auto scroll-smooth pt-10 pb-20 flex flex-col gap-5 items-center mt-20">
+          <input
+            placeholder="Give me a name"
+            className=" px-7 text-2xl font-semibold outline-none rounded-lg"
+          />
+          <div
+            role="button"
+            className="w-full text-slate-500 flex flex-col gap-3 justify-center items-center min-h-60 border-2 border-dashed rounded-xl"
+          >
+            <img width={80} src={picPlaceholder} alt="pic-placeholder" />
+            <p className="text-sm underline decoration-green-500">Browse</p>
+          </div>
+          <textarea
+            placeholder="Write what went into this design or add any details you'd like to mention."
+            className="w-4/5 px-3 py-4 outline-none border-2 border-white hover:border-inherit focus:border-purple-300 rounded-xl"
+          />
         </div>
-        <textarea
-          placeholder="Write what went into this design or add any details you'd like to mention."
-          rows={3}
-          className="w-1/3 p-3 outline-none border focus:border-zinc-500 rounded-lg"
-        />
       </div>
     </div>
   );
