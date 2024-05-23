fc9147b8056409bc6e07b6d661f94df358e4db02
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-05-23 19:56:57 +0530

    profile pic not displaying

--- client/src/components/PostContainer.jsx
+++ client/src/components/PostContainer.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import PostDetails from "./PostDetails";
 import { getPost } from "@/actions/postActions";
+import ClickedPost from "./ClickedPost";
 
-const PostContainer = ({ title, postId, creatorId, artImg }) => {
+const PostContainer = ({ title, postId, creatorId, artImg, creatorPic }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -20,27 +20,33 @@ const PostContainer = ({ title, postId, creatorId, artImg }) => {
   };
 
   return (
-    <div className="flex flex-col gap-2 w-full mb-5 break-inside-avoid">
+    <div className="flex flex-col gap-1.5 w-full mb-8 break-inside-avoid">
       <Drawer>
         <DrawerTrigger>
           <img
             onClick={openPost}
             src={artImg}
-            className="max-w-full rounded-xl"
+            className="max-w-full rounded-2xl"
             alt="art"
             loading="lazy"
           />
         </DrawerTrigger>
         <DrawerContent className="h-[91%]">
-          <PostDetails />{" "}
+          <ClickedPost />{" "}
         </DrawerContent>
       </Drawer>
-      <p className="font-medium text-sm">{title}</p>
-      <div className="cursor-pointer flex gap-2 items-center">
-        <img className="w-8 h-8" alt="profile-pic" />
-        <p onClick={viewProfile} className="hover:underline text-sm">
-          Artist
-        </p>
+      <div className="px-2 flex flex-col gap-1">
+        <p className="font-medium text-sm">{title}</p>
+        <div className="cursor-pointer flex gap-1 items-center">
+          <img
+            className="w-8 h-8 rounded-full"
+            src={creatorPic}
+            alt="profile-pic"
+          />
+          <p onClick={viewProfile} className="hover:underline text-sm">
+            Rohan
+          </p>
+        </div>
       </div>
     </div>
   );
