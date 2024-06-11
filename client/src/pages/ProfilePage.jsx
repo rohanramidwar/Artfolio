67cc25c851bacf4a8b618700526917c46801de4b
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-06-11 21:00:28 +0530

    add profile page

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,23 +1,62 @@
-import { getProfile } from "@/actions/postActions";
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const ProfilePage = () => {
-  const dispatch = useDispatch();
+  const location = useLocation();
 
-  const { id } = useParams(); //artist id
-
+  //to get user id
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
   useEffect(() => {
-    dispatch(getProfile(id));
-  }, [dispatch, id]);
-
-  const { artist } = useSelector((state) => state?.posts);
+    setUser(JSON.parse(localStorage.getItem("profile")));
+  }, [location]);
 
   return (
-    <div>
-      ProfilePage
-      {artist?.displayName}
+    <div className="px-14 py-5 text-slate-900">
+      <div className="flex flex-col items-center gap-3">
+        <img
+          className="w-[120px] h-[120px] rounded-full"
+          src={user?.profilePic}
+          alt="user"
+        />
+        <p className="text-4xl font-semibold">{user?.displayName}</p>
+        <div className="flex gap-3">
+          {/* <Button className="rounded-xl h-auto py-3 bg-zinc-100 hover:bg-zinc-100 text-slate-900">
+            Contact
+          </Button>
+          <Button className="rounded-xl h-auto py-3 bg-violet-500 hover:bg-violet-500">
+            Follow
+          </Button> */}
+          <Button className="rounded-xl h-auto py-3 bg-zinc-100 hover:bg-zinc-100 text-slate-900">
+            Edit profile
+          </Button>
+        </div>
+      </div>
+      <div className="mt-16 flex gap-3">
+        <Button className="rounded-xl h-auto py-3 bg-zinc-100 hover:bg-zinc-100 text-slate-900">
+          Created
+        </Button>
+        <Button className="rounded-xl h-auto py-3 bg-inherit hover:bg-zinc-100 text-slate-900">
+          Saved
+        </Button>
+      </div>
+      <div className="py-12 sm:grid flex flex-col sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {user?.posts?.map((post) => (
+          <div
+            key={post?._id}
+            className="flex justify-center items-center group relative w-full h-full"
+          >
+            <img
+              className="border w-full h-60 object-cover rounded-2xl"
+              src={post?.artImg}
+              alt="art"
+            />
+            <div className="hidden group-hover:flex profile_card-title">
+              <p className="w-full">{post?.title}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
