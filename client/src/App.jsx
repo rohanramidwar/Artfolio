1ad41978ba593d7932b146575651c685c8a8a5c5
Rohan Ramidwar <rohanramidwar71@gmail.com>
2024-09-03 01:29:52 +0530

    change font

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import "@fontsource/inter";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/700.css";
-import "@fontsource/inter/800.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import ProfilePage from "./pages/ProfilePage";
